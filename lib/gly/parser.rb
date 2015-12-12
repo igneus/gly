ea9b16733bc91aae1e4535cd545854862fc45faf
Jakub Pavlík jn <severus@post.cz>
2015-12-12 12:59:46 +0100

    lyric detection by nonmusical letters: more letters

--- lib/gly/parser.rb
+++ lib/gly/parser.rb
@@ -64,7 +64,7 @@ module Gly
 
     def contains_unmusical_letters?(str)
       letters = str.gsub(/[\W\d_]+/, '')
-      letters !~ /\A[a-mvoz]*\Z/i # incomplete gabc music letters!
+      letters !~ /\A[a-mvoxz]*\Z/i # incomplete gabc music letters!
     end
 
     def parse_header(str)
