cf7edbd038fe4d22524e1936bc9f7f46aba7e4e3
Jakub Pavlik <severus@post.cz>
2026-08-24 21:13:58 +0200

    Emacs mode: support automatic un/commenting
    with commands like comment-line or comment-region

--- elisp/gly-mode.el
+++ elisp/gly-mode.el
@@ -2,10 +2,9 @@
 
 (define-generic-mode
   'gly-mode             ;; name of the mode
-  '()                   ;; comments delimiter
+  '(?%)                 ;; comments delimiter
   '()                   ;; some keywords
   '(("\\\\\\(score\\|header\\|lyrics\\|l\\|music\\|markup\\|m\\)" . 'font-lock-keyword-face)
-    ("\\(%.*\\)" 1 'font-lock-comment-face)
     ("^[a-z_\\-]+:" . 'font-lock-builtin-face) ;; header fields
     ("^.*--.*" . 'font-lock-type-face) ;; lyrics line
     )
